c99a8f87a8d5459b2d118d0fa489511dfea98c24
pancake~ <834430775@qq.com>
2022-09-12 22:50:16 +0800

    init

---
+++ src/test/java/reggie_take_out_package/ReggieTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package reggie_take_out_package;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
